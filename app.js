7a8a76e7d502aff1ebe0760ad9653a1478c4d397
unknown <abhijitpadhy2000@gmail.com>
2024-05-03 09:22:24 +0530

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const { mongoUrl } = require("./keys");
 const cors = require("cors");
 const contactRoutes = require("./models/contactModel");
 
+dotenv.config();
 // Enable CORS middleware
 app.use(cors());
+app.use(express.json());
 
 require('./models/model')
 require('./models/post')
@@ -15,6 +17,7 @@ app.use(express.json())
 app.use(require("./routes/auth"))
 app.use(require("./routes/createPost"))
 app.use(require("./routes/contactRoutes"));
+const port = process.env.PORT || 5000;
 mongoose.connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
